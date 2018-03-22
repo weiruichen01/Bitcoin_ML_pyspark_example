b7fca640adae4aacdefa0850ecbad1700eac92f4
AC <27918980+acsets@users.noreply.github.com>
2018-03-22 19:42:54 +0800

    Update get_block_content.sh
    comment added

--- get_block_content.sh
+++ get_block_content.sh
@@ -2,8 +2,16 @@
 #modify the number to get different range of block data
 for i in $(seq 500000 513357)
 do
-        bitcoin-cli getblock $(bitcoin-cli getblockhash "$i") > ~/block_data/$i
+        bitcoin-cli getblock $(bitcoin-cli getblockhash "$i") > ~/<some_path>/$i
 done
 
+#getblockhash
+#input: block height
+#output: block hash
 
-#
+#getblock
+#input: block hash
+#output: block content
+
+# > will redirect data from left hand side to a text file
+# put file name on the right hand side of >
