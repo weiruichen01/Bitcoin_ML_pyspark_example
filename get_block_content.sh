5ffe4ab6ca3699610e9010a0c81b99888d8df6b9
AC <27918980+acsets@users.noreply.github.com>
2018-03-22 19:38:54 +0800

    Update get_block_content.sh

--- get_block_content.sh
+++ get_block_content.sh
@@ -1,4 +1,8 @@
+#get back the content from block no.500000 to block no.513357
+#modify the number to get different range of block data
 for i in $(seq 500000 513357)
 do
         bitcoin-cli getblock $(bitcoin-cli getblockhash "$i") > ~/block_data/$i
 done
+
+
